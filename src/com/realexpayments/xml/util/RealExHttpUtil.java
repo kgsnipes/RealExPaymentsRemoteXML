e3c2843c26af8a96cc535ef29799517a67abad61
kaushik ganguly <kgsnipes@gmail.com>
2014-01-20 16:55:06 +0530

    adding http request

--- src/com/realexpayments/xml/util/RealExHttpUtil.java
+++ src/com/realexpayments/xml/util/RealExHttpUtil.java
@@ -1,13 +1,20 @@
 package com.realexpayments.xml.util;
 
+import org.apache.http.client.fluent.Request;
+import org.apache.http.entity.ContentType;
+
+import com.realexpayments.request.RealExRequest;
 import com.realexpayments.response.RealExResponse;
-import com.realexpayments.xml.bean.RealExBean;
 
 public class RealExHttpUtil {
 	
-	public static RealExResponse getResponse(RealExBean request)
+	public static RealExResponse getResponse(String url,RealExRequest request)throws Exception
 	{
+		RealExResponse retVal=null;
 		
+		retVal=RealExBeanToXMLConverterUtil.toResponseBean(Request.Post(url).bodyString(RealExBeanToXMLConverterUtil.toXML(request), ContentType.APPLICATION_XML).execute().returnContent().asString());
+		
+		return retVal;
 	}
 
 }
