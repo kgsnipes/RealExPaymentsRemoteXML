b926be689d9bc9fa9cede6e85cf01108308ae6fe
kaushik ganguly <kgsnipes@gmail.com>
2014-01-22 16:17:51 +0530

    removing jaxb

--- src/com/realexpayments/request/jaxb/RealExRequest.java
+++
@@ -1,7 +0,0 @@
-package com.realexpayments.request.jaxb;
-
-import com.realexpayments.xml.bean.RealExBean;
-
-public class RealExRequest extends RealExBean {
-
-}
--- src/com/realexpayments/xml/bean/jaxb/RealExBean.java
+++
@@ -1,7 +0,0 @@
-package com.realexpayments.xml.bean.jaxb;
-
-import java.io.Serializable;
-
-public class RealExBean implements Serializable{
-
-}
